c7c6a32ac1548905bfc23ce2aaab0adea6bc3f60
Tomás Maiza <tomimaiza@gmail.com>
2024-06-07 10:09:38 -0300

    Actualizo archivos

--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -10,7 +10,7 @@ singleton x = [x]
 
 length' :: [a] -> Int
 length' [] = 0
-length' (x:xs) = 1 + length' xs
+length' (x:xs) = length xs
 
 nth :: [a] -> Int -> a
 nth [] _ = error "Lista vacia o indice invalido"
@@ -25,13 +25,10 @@ tabulate f n | n < 0 = error "Argumento inválido"
                                                  | otherwise = (f i) : (tabulate_aux f (i + 1) m)
 
 map' :: (a -> b) -> [a] -> [b]
-map' f [] = []
-map' f (x:xs) = (f x) : (map' f xs)
+map' f xs = map f xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' p [] = []
-filter' p (x:xs) | p x = x : (filter' p xs)
-                 | otherwise = filter' p xs
+filter' p xs = filter p xs
 
 append :: [a] -> [a] -> [a]
 append [] ys = ys
@@ -39,15 +36,10 @@ append xs [] = xs
 append (x:xs) ys = x : (append xs ys)
 
 take' :: [a] -> Int -> [a]
-take' [] _ = []
-take' xs 0 = []
-take' (x:xs) n | n < 0 = error "Argumento invalido"
-               | otherwise = x : (take' xs (n - 1))
+take' xs n = take n xs
 
 drop' :: [a] -> Int -> [a]
-drop' [] _ = []
-drop' ls@(x:xs) n | n <= 0 = ls
-                  | otherwise = drop' xs (n - 1)
+drop' xs n = drop n xs
 
 showt :: [a] -> TreeView a [a]
 showt [] = EMPTY
@@ -84,7 +76,7 @@ reduce f e xs = f e (reduceT f (toTree xs))
 
 
 scan :: (a -> a -> a) -> a -> [a] -> ([a], a)
-scan f e s = let (ys, total) = (scanAux f e s)
+scan f e s = let (ys, total) = scanAux f e s
                  sec = completar f s ys 0
              in (sec, total)
               where
@@ -92,22 +84,28 @@ scan f e s = let (ys, total) = (scanAux f e s)
                 completar f xs [] _ = []
                 completar f [x] (y:ys) i = case even i of
                                                 True -> y : (completar f [x] (y:ys) (i + 1))
-                                                False -> [f y x]
+                                                False -> []
                 completar f (x:x':xs) (y:ys) i = case even i of
                                                 True -> y : (completar f (x:x':xs) (y:ys) (i + 1))
                                                 False -> (f y x) : (completar f xs ys (i + 1))
 
-scanAux :: (a -> a -> a) -> a -> [a] -> ([a], a)
-scanAux f e [x,y] = ([e, (f e x)], (f e (f x y)))
-scanAux f e s = scanAux f e (contraccion f s)
-              where
-                contraccion f [] = []
-                contraccion f [x] = [x]
-                contraccion f (x:y:xs) = let (res, contra) = (f x y) ||| (contraccion f xs)
-                                         in res : contra
+scanAux f e s = let sec = (reduccion f (e: e:(contraccion f s)))
+                    len = length' sec
+                    (secFinal, total) = (take' sec (len - 1)) ||| nth sec (len - 1)
+                in (secFinal, total)
+            
+contraccion :: (a -> a -> a) -> [a] -> [a]            
+contraccion f [] = []
+contraccion f [x] = [x]
+contraccion f (x:y:xs) = let (res, contra) = (f x y) ||| (contraccion f xs)
+                         in res : contra
+
+reduccion :: (a -> a -> a) -> [a] -> [a]
+reduccion f [] = []
+reduccion f [x] = [x]
+reduccion f (x:y:xs) = res : (reduccion f (res:xs))  
+      where res = (f x y)
 
 
-{-
-fromList   :: [a] -> s a
--}
-
+fromList :: [a] -> [a] 
+fromList xs = xs
