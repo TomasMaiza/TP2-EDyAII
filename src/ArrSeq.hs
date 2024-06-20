25417064f75ee434225421f1a1497b28bbc42a26
Tomás Maiza <tomimaiza@gmail.com>
2024-06-20 09:38:13 -0300

    Corrijo implementaciones

--- src/ArrSeq.hs
+++ src/ArrSeq.hs
@@ -1,5 +1,5 @@
 module ArrSeq where
-import Prelude hiding (length)
+import Prelude hiding (length, map, filter, take, drop)
 import qualified Arr as A
 import Arr (Arr, (!), length, subArray, tabulate, empty, flatten)
 
@@ -9,58 +9,44 @@ import Par
 singleton :: a -> Arr a
 singleton x = A.fromList [x]
 
-length' :: Arr a -> Int
-length' xs = length xs 
-
 nth :: Arr a -> Int -> a
 nth xs i = xs ! i
 
-tabulate' :: (Int -> a) -> Int -> Arr a
-tabulate' f n = tabulate f n
+map :: (a -> b) -> Arr a -> Arr b
+map f xs = tabulate (\i -> f (xs ! i)) (length xs)
 
-map' :: (a -> b) -> Arr a -> Arr b
-map' f xs = tabulate (\i -> f (xs ! i)) (length xs)
-
-filter' :: (a -> Bool) -> Arr a -> Arr a
-filter' p xs = flatten (map' (\x -> if p x then singleton x else empty) xs)
+filter :: (a -> Bool) -> Arr a -> Arr a
+filter p xs = flatten (map (\x -> if p x then singleton x else empty) xs)
 
 append :: Arr a -> Arr a -> Arr a
 append xs ys = let (lenxs, lenys) = length xs ||| length ys
                    n = lenxs + lenys
                in tabulate (\i -> if i < lenxs then xs ! i else ys ! (i - lenxs)) n
 
-take' :: Arr a -> Int -> Arr a
-take' xs n = subArray 0 n xs
+take :: Arr a -> Int -> Arr a
+take xs n = subArray 0 n xs
 
-drop' :: Arr a -> Int -> Arr a
-drop' xs n = subArray n (length xs - n) xs
+drop :: Arr a -> Int -> Arr a
+drop xs n = subArray n (length xs - n) xs
 
 showt :: Arr a -> TreeView a (Arr a)
 showt xs = case length xs of
             0 -> EMPTY
             1 -> ELT (xs ! 0)
-            n -> NODE (take' xs tam) (drop' xs tam) where tam = div n 2
+            n -> NODE t d where tam = div n 2
+                                (t, d) = take xs tam ||| drop xs tam
 
 showl :: Arr a -> ListView a (Arr a)
 showl xs = case length xs of
             0 -> NIL
-            _ -> CONS (xs ! 0) (drop' xs 1)
-{-
-reduce :: (a -> a -> a) -> a -> Arr a -> a
-reduce f e ls = case showt ls of 
-                  EMPTY -> e
-                  _ -> f e (reduce_aux ls)
-                        where
-                            reduce_aux s = case showt s of
-                                              ELT x -> x
-                                              NODE l r -> let (l', r') = reduce_aux l ||| reduce_aux r
-                                                          in f l' r'-}
+            _ -> CONS (xs ! 0) (drop xs 1)
 
 showt' :: Arr a -> TreeView a (Arr a)
 showt' xs = case length xs of
             0 -> EMPTY
             1 -> ELT (xs ! 0)
-            n -> NODE (take' xs tam) (drop' xs tam) where tam = 2 ^ ((floor . logBase 2) ((fromIntegral n)-1))
+            n -> NODE t d where tam = 2 ^ ((floor . logBase 2) ((fromIntegral n)-1))
+                                (t, d) = take xs tam ||| drop xs tam
 
 reduce :: (a -> a -> a) -> a -> Arr a -> a
 reduce f e ls = case showt' ls of 
@@ -81,13 +67,13 @@ scan f e s = let (ys, total) = scanAux f e s
                 completar f xs ys i | length xs == 0 || length ys == 0 = empty
                                     | otherwise = case even i of
                                                     True -> append (singleton (ys ! 0)) (completar f xs ys (i + 1))
-                                                    False -> if length xs == 1 then empty else append (singleton (f (ys ! 0) (xs ! 0))) (completar f (drop' xs 2) (drop' ys 1) (i + 1))
+                                                    False -> if length xs == 1 then empty else append (singleton (f (ys ! 0) (xs ! 0))) (completar f (drop xs 2) (drop ys 1) (i + 1))
 
 scanAux :: (a -> a -> a) -> a -> Arr a -> (Arr a, a)
 scanAux f e s = let e' = singleton e
                     sec = (reduccion f (append e'(append e' (contraccion f s))))
                     len = length sec
-                    (secFinal, total) = (take' sec (len - 1)) ||| nth sec (len - 1)
+                    (secFinal, total) = take sec (len - 1) ||| nth sec (len - 1)
                 in (secFinal, total)
 
 contraccion :: (a -> a -> a) -> Arr a -> Arr a            
@@ -95,8 +81,7 @@ contraccion f ls = case length ls of
                     0 -> empty
                     1 -> ls
                     _ -> let (x, y) = ls ! 0 ||| ls ! 1
-                             xs = drop' ls 2
-                             op = f x y
+                             (xs, op) = drop ls 2 ||| f x y
                              (sing, ys) = singleton op ||| contraccion f xs
                          in append sing ys
 
@@ -105,7 +90,7 @@ reduccion f ls = case length ls of
                     0 -> empty
                     1 -> ls
                     _ -> let (x, y) = ls ! 0 ||| ls ! 1
-                             xs = drop' ls 2
+                             xs = drop ls 2
                              sing = singleton (f x y)
                              ys = append sing xs
                              red = reduccion f ys
@@ -118,11 +103,11 @@ instance Seq Arr where
   lengthS = length
   nthS = nth
   tabulateS = tabulate
-  mapS = map'
-  filterS = filter'
+  mapS = map
+  filterS = filter
   appendS = append
-  takeS    = take'
-  dropS    = drop'
+  takeS    = take
+  dropS    = drop
   showtS   = showt
   showlS   = showl
   joinS    = flatten
--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -23,11 +23,16 @@ tabulate f n | n < 0 = error "Argumento inválido"
                               tabulate_aux f i m | i == m = []
                                                  | otherwise = let (fx, rest) = (f i) ||| (tabulate_aux f (i + 1) m)
                                                                in fx : rest
+
 map' :: (a -> b) -> [a] -> [b]
-map' f xs = map f xs
+map' f [] = []
+map' f (x:xs) = let (fx, rest) = f x ||| map' f xs
+                in fx : rest
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' p xs = filter p xs
+filter' p [] = []
+filter' p (x:xs) = let (px, rest) = p x ||| filter' p xs
+                   in if px then x : rest else rest
 
 append :: [a] -> [a] -> [a]
 append [] ys = ys
@@ -46,7 +51,7 @@ showt [x] = ELT x
 showt xs = let len = length' xs
                s = div len 2
                (t1, t2) = take' xs s ||| drop' xs s
-           in (NODE t1 t2)
+           in NODE t1 t2
 
 showl :: [a] -> ListView a [a]
 showl [] = NIL
@@ -55,24 +60,6 @@ showl (x:xs) = CONS x xs
 join :: [[a]] -> [a]
 join [] = []
 join (xs:xss) = append xs (join xss)
-
-{-
-data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
-
-reduce :: (a -> a -> a) -> a -> [a] -> a
-reduce f e [] = e
-reduce f e xs = f e (reduceT f (toTree xs))
-      where 
-        toTree ls@(x:xs) = case length' ls of
-                           1 -> Leaf x
-                           n -> let m = (fromIntegral n)
-                                    pp = 2 ^ ((floor . logBase 2) (m-1))
-                                    (l, r) = toTree (take' ls pp) ||| toTree (drop' ls pp)
-                                in Node l r
-
-        reduceT f (Leaf x) = x
-        reduceT f (Node l r) = let (l', r') = reduceT f l ||| reduceT f r
-                               in f l' r' -}
 
 reduce :: (a -> a -> a) -> a -> [a] -> a
 reduce f e [] = e
@@ -84,7 +71,8 @@ reduce f e ls = let red = reduce_aux ls
                   where
                     reduce_aux [] = []
                     reduce_aux [x] = [x] 
-                    reduce_aux (x:y:xs) = (f x y) : (reduce_aux xs)
+                    reduce_aux (x:y:xs) = let (op, reduc) = (f x y) ||| (reduce_aux xs)
+                                          in op : reduc
 
 
 scan :: (a -> a -> a) -> a -> [a] -> ([a], a)
@@ -99,11 +87,13 @@ scan f e s = let (ys, total) = scanAux f e s
                                                 False -> []
                 completar f (x:x':xs) (y:ys) i = case even i of
                                                 True -> y : (completar f (x:x':xs) (y:ys) (i + 1))
-                                                False -> (f y x) : (completar f xs ys (i + 1))
+                                                False -> let (op, rest) = (f y x) ||| (completar f xs ys (i + 1))
+                                                         in op : rest
 
+scanAux :: (a -> a -> a) -> a -> [a] -> ([a], a)
 scanAux f e s = let sec = (reduccion f (e: e:(contraccion f s)))
                     len = length' sec
-                    (secFinal, total) = (take' sec (len - 1)) ||| nth sec (len - 1)
+                    (secFinal, total) = take' sec (len - 1) ||| nth sec (len - 1)
                 in (secFinal, total)
             
 contraccion :: (a -> a -> a) -> [a] -> [a]            
@@ -125,8 +115,8 @@ instance Seq [] where
   lengthS = length
   nthS = nth
   tabulateS = tabulate
-  mapS = map
-  filterS = filter
+  mapS = map'
+  filterS = filter'
   appendS = append
   takeS = take'
   dropS = drop'
